821ecef949039620f2e108d519e535dc18ed5669
mwgkgk <mwgkgk@gmail.com>
2016-12-29 21:27:33 +0200

    Snippets & Skeletons updates

--- vim/skeletons/skeleton.sh
+++ vim/skeletons/skeleton.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-    echo $(basename $0)
-elif [ $# -gt 1 ]; then
-    echo $(basename $0)
+    echo $(basename $0) $(pwd)
+elif [ $# -ge 1 ]; then
+    for arg in "$@"; do
+        echo $(basename $0) $arg
+    done
 else 
     exit
 fi
