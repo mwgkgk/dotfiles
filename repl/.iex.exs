3bfd5da22c2a0d72369b0a1f3ddbbe22fc0bee32
mwgkgk <mwgkgk@gmail.com>
2017-06-25 17:52:59 +0300

    Add .iex.exs

---
+++ repl/.iex.exs
@@ -0,0 +1,26 @@
+# IEx.configure colors: [eval_result: [:cyan, :bright]]
+
+# IEx.configure colors: [
+#   eval_info: [:yellow, :normal],
+#   eval_result: [:cyan, :normal],
+#   stack_app: [:green, :normal],
+#   stack_info: [:white, :normal]
+# ]
+
+IEx.configure colors: [enabled: true]
+
+# alias IEx.Helpers.recompile r
+
+import Pouch
+IO.puts ~S"""
+       .       ______    .      
+         _____/      \____     .
+     .--/    /  /  \  \    \--.    * & ^ * * * * * * * * * ^ & ^ * * * * * * * * * ^ & *
+ .  (  ( << (  <    >  ) >> )  )   * & ^ * S e t t i n g * ^ & ^ * u p * * * * * * ^ $ *
+     ^--\____\  \  /  /____/--^    * & ^ * * * B a s e * * ^ & ^ * s y s t e m s * ^ $ *
+              \______/             * & ^ * * * * * * * * * ^ & ^ * * * * * * * * * ^ & *
+       .                     *
+              /   o   \
+             /   /)\   \
+            /    (\     \
+"""
