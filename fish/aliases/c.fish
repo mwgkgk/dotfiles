6917fda5bf31796426cab55d5d17318507ed3b58
mwgkgk <mwgkgk@gmail.com>
2017-12-13 18:36:30 +0200

    Update c (non-recursive yet)

--- fish/aliases/c.fish
+++ fish/aliases/c.fish
@@ -1,4 +1,13 @@
 # Defined in - @ line 0
 function c --description 'alias c cd'
-	cd  $argv;
+  if test -d $argv[1]
+    cd $argv[1]
+  else if test -d "$HOME/code/$argv[1]"
+    cd "$HOME/code/$argv[1]"
+  else
+    set -l matching_dirs (find ./ -maxdepth 1 -type d -iname "*$argv[1]*")
+    if test (count $matching_dirs) = 1
+      cd $matching_dirs[1]
+    end
+  end
 end
