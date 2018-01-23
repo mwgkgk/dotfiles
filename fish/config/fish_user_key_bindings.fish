ecf97cefc54afe460de6192f48133103a8ea0919
mwgkgk <mwgkgk@gmail.com>
2018-01-23 13:10:13 +0200

    !! and !$ for fish

---
+++ fish/config/fish_user_key_bindings.fish
@@ -0,0 +1,23 @@
+function bind_bang
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function bind_dollar
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
+function fish_user_key_bindings
+  bind ! bind_bang
+  bind '$' bind_dollar
+end
