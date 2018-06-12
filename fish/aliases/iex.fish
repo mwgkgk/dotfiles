b41f90f716e2f135c7efaa776dd7fa80c6343668
mwgkgk <mwgkgk@gmail.com>
2018-06-13 02:33:56 +0300

    New path

--- fish/aliases/iex.fish
+++ fish/aliases/iex.fish
@@ -1,3 +1,3 @@
 function iex
-  /usr/bin/iex -pa ~/code/elixir/pouch/_build/dev/lib/pouch/ebin --erl '-kernel shell_history enabled' $argv
+  /usr/bin/iex -pa ~/ruins/elixir/pouch/_build/dev/lib/pouch/ebin --erl '-kernel shell_history enabled' $argv
 end
