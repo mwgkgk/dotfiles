47072b3baee0eb1cda4f48995811fc5888df2945
mwgkgk <mwgkgk@gmail.com>
2018-10-24 09:56:22 +0300

    Add OPAM init

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
 set -x PATH ~/bin ~/.cargo/bin $PATH
 
 eval (perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
+
+# OPAM configuration
+source /home/mitnick/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
