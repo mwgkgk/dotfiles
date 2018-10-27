4b91cc0454738904a9936ef7efa5c7e735aeef60
mwgkgk <mwgkgk@gmail.com>
2018-10-27 12:00:13 +0300

    Run tmux rename-window from fish_title

--- fish/config/fish_title.fish
+++ fish/config/fish_title.fish
@@ -1,3 +1,12 @@
 function fish_title
+  set -l job_count (jobs -c | wc -l)
+  if test $job_count -eq 0
+    if [ "fish" != $_ ]
+      tmux rename-window "$argv" 
+    else
+      tmux rename-window (prompt_pwd)"/\$"
+    end
+  end
+
   echo $_
 end
