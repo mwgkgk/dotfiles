50b9a4a343f0c5e88a1c7959abef547d6b10e315
mwgkgk <mwgkgk@gmail.com>
2018-11-03 16:22:52 +0200

    Turn off perl local lib

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,7 @@ set -x PATH ~/bin ~/.cargo/bin $PATH
 set -x FZF_DEFAULT_COMMAND "fd --type f --follow --hidden --exclude .git"
 set -x FZF_DEFAULT_OPTS ""
 
-eval (perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
+# eval (perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
 
 # OPAM configuration
 source /home/mitnick/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
