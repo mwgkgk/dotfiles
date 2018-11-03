70d4cbd7930826564ff7cfc95c64fb4e0b57ade4
mwgkgk <mwgkgk@gmail.com>
2018-11-03 16:23:57 +0200

    Update colors

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,8 @@
 set -x PATH ~/bin ~/.cargo/bin $PATH
 
+set -x fish_color_cwd af8787
+set -x fish_command_timer_color 9F7010
+
 set -x FZF_DEFAULT_COMMAND "fd --type f --follow --hidden --exclude .git"
 set -x FZF_DEFAULT_OPTS ""
 
