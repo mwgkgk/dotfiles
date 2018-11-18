693a448077f6b58fac1981d027f8e63ce45bb544
mwgkgk <mwgkgk@gmail.com>
2018-11-18 08:19:37 +0200

    Skeletons & snippets

--- vim/skeletons/python.py
+++ vim/skeletons/python.py
@@ -1,11 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
 
 import sys
+
 
 def main():
     args = sys.argv[1:]
     print(args)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
