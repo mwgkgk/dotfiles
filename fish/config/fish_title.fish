69b55c15079c793b3222cdbe871dbc16b8344abc
mwgkgk <mwgkgk@gmail.com>
2018-12-06 11:22:12 +0200

    Add `test -n $TMUX`

--- fish/config/fish_title.fish
+++ fish/config/fish_title.fish
@@ -2,9 +2,9 @@ function fish_title
   set -l job_count (jobs -c | wc -l)
   if test $job_count -eq 0
     if [ "fish" != $_ ]
-      tmux rename-window "$argv" 
+      test -n $TMUX ;and tmux rename-window "$argv" 
     else
-      tmux rename-window (prompt_pwd)"/\$"
+      test -n $TMUX ;and tmux rename-window (prompt_pwd)"/\$"
     end
   end
 
