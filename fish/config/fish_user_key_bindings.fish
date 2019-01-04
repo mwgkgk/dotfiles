30da6493e05e20c462568e47e490fb2c29b8adfd
mwgkgk <mwgkgk@gmail.com>
2019-01-04 05:45:31 +0200

    Change M-x to fdz-ink

--- fish/config/fish_user_key_bindings.fish
+++ fish/config/fish_user_key_bindings.fish
@@ -188,8 +188,8 @@ function bind_fdz_dir_cases
     commandline -f repaint
 end
 
-function bind_fdz_dir_ink
-    fdz-dir ~/ink | read -l result
+function bind_fdz_ink
+    fdz-ink | read -l result
     and cd $result
     commandline -f repaint
 end
@@ -409,8 +409,8 @@ function fish_user_key_bindings
     bind ó bind_fdz_dir_shop
     # M-c to fdz-dir ~/shop/cases/
     bind ã bind_fdz_dir_cases
-    # M-x to fdz-dir ~/ink/
-    bind ø bind_fdz_dir_ink
+    # M-x to fdz-ink
+    bind ø bind_fdz_ink
     # M-w to fdz-dir .
     bind ÷ bind_fdz_dir_here
 
