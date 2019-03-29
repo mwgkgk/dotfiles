4328152194ee9b6d9d0fe978ae1ed2515fef3b00
mwgkgk <mwgkgk@gmail.com>
2019-03-30 01:59:51 +0200

    Add repl/profiles.clj

--- repl/profiles.clj
+++ repl/profiles.clj
@@ -1,4 +1,8 @@
 {:user 
  {:dependencies
   [[cljfmt "0.6.4"]
+
+   ;; Provides:
+   ;; - (load-project) to load new deps from project.clj
+   ;; - (alembic.still/distill '[org.clojure/tools.logging "0.3.2"])
    [alembic "0.3.2"]]}}
