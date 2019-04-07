7ae375d68896171abaa0e4da0c2a45d367deb8b7
mwgkgk <mwgkgk@gmail.com>
2019-04-07 05:26:31 +0300

    use-package company

--- emacs/init.el
+++ emacs/init.el
@@ -105,9 +105,25 @@
 ;;; Completion
 
 
-(use-package auto-complete
+(use-package company
+  :defer 3
   :config
-  (ac-config-default))
+  (setq company-show-numbers t)
+  (setq company-dabbrev-ignore-case nil)
+  (setq company-dabbrev-downcase nil)
+  (setq company-tooltip-align-annotations t)
+
+  (global-company-mode +1))
+
+(use-package company-prescient
+             :demand t
+             :after company
+             :config
+             (company-prescient-mode +1))
+
+
+;;; Auto-pair
+
 
 (electric-pair-mode 1)
 ; Doesn't work
