d5c8b69ad8f88695c8adc77cb71a29b010d706e3
mwgkgk <mwgkgk@gmail.com>
2019-09-21 03:16:01 +0300

    Update sbcl --script skeleton

--- vim/skeletons/common.lisp
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/sbcl --script
-
-(defun main ()
-  (format t "hello, world"))
-
-(main)
---
+++ vim/skeletons/sbcl-script.lisp
@@ -0,0 +1,7 @@
+; #!/usr/bin/sbcl --script
+
+(defun mn (args)
+  nil)
+
+(defun main ()
+  (mn *posix-argv*))
