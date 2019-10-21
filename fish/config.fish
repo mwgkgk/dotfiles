a80d2fcc41800c496d97de316ecaa25c89b08324
mwgkgk <mwgkgk@gmail.com>
2019-10-21 22:55:33 +0300

    Disable fish-command-timer

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,7 @@ umask 0007
 
 set -x fish_color_cwd CEB982
 set -x fish_color_prompt C28B5B
-set -x fish_command_timer_color AF875F
+# set -x fish_command_timer_color AF875F
 
 set -x FZF_DEFAULT_COMMAND "fd --type f --follow --hidden --exclude .git"
 set -x FZF_DEFAULT_OPTS " --bind 'ctrl-a:select-all+accept,tab:print-query'"
