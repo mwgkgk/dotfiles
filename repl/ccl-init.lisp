8d0734681adc427b84a75a7d0a210b3eb01be280
mwgkgk <mwgkgk@gmail.com>
2019-11-22 09:18:44 +0200

    Compile & load new asdf version

---
+++ repl/ccl-init.lisp
@@ -0,0 +1,16 @@
+;;; The following lines added by ql:add-to-init-file:
+#-quicklisp
+(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
+                                       (user-homedir-pathname))))
+  (when (probe-file quicklisp-init)
+    (load quicklisp-init)))
+
+;;; Use a newer asdf version:
+(let
+  ((compiled-file #P"~/.cache/local/common-lisp/ccl/asdf.lx64fsl"))
+  (progn
+    (ensure-directories-exist (directory-namestring compiled-file))
+    (if (not (probe-file compiled-file))
+      (compile-file #P"~/asdf.lisp"
+                    :output-file compiled-file))
+    (load compiled-file)))
