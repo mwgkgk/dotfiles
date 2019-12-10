43cd88ac8fde108b7f99966bd2e0eb13fbc97aca
mwgkgk <mwgkgk@gmail.com>
2019-12-10 08:57:55 +0200

    Simple bash skeleton

--- vim/skeletons/bash.sh
+++ vim/skeletons/bash.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
-    echo $(basename $0) $(pwd)
-elif [ $# -ge 1 ]; then
-    for arg in "$@"; do
-        echo $(basename $0) $arg
-    done
-else 
-    exit
-fi
+set -o errexit
+set -o nounset
+set -o pipefail
