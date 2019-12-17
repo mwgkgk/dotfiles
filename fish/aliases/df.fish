544d378d31954bae4048d216d9f9c678a328ba01
mwgkgk <mwgkgk@gmail.com>
2019-12-17 23:31:41 +0200

    Use native :df in Vifm's <M-o>

--- fish/aliases/df.fish
+++ fish/aliases/df.fish
@@ -1,3 +1,5 @@
 function df
-  pydf $argv
+  # Reason this uses --bw is because vifm's built-in :df command is
+  # triggering this alias, and vifm renders the color escape sequences as is.
+  pydf $argv --bw
 end
