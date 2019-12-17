3339d361c92cff6aefc02b1b1b80f829b77d9d93
mwgkgk <mwgkgk@gmail.com>
2019-12-18 01:02:37 +0200

    Update :df command

--- fish/aliases/df.fish
+++ fish/aliases/df.fish
@@ -1,5 +1,3 @@
 function df
-  # Reason this uses --bw is because vifm's built-in :df command is
-  # triggering this alias, and vifm renders the color escape sequences as is.
-  pydf $argv --bw
+  pydf $argv
 end
