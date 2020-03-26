5ccfff12277f557ff2182d0da859262615b2ff53
mwgkgk <mwgkgk@gmail.com>
2020-03-26 08:37:16 +0200

    Disable fish_greeting for lisp-adv

--- fish/config/fish_greeting.fish
+++ fish/config/fish_greeting.fish
@@ -1,7 +1,8 @@
 function fish_greeting
     if test -z "$VIM_SERVERNAME" &&
         not test -z "$CHI" &&
-        not test "$CHI" = "rogue"
+        not test "$CHI" = "rogue" &&
+        not test "$LISP_ADV"
         servus
     end
 end
