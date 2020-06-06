6ad3632df7d85cbd9d8eed63804fa3e925222cc8
mwgkgk <mwgkgk@gmail.com>
2020-06-07 02:36:46 +0300

    colo science

--- fish/config.fish
+++ fish/config.fish
@@ -2,9 +2,15 @@ set -x PATH ~/bin ~/.local/bin ~/.cargo/bin ~/.node_modules/bin ~/go/bin $PATH
 
 umask 0007
 
-set -x fish_color_prompt B5FFCE
-set -x fish_color_cwd 7CFFAA
-set -x fish_command_timer_color 82826E
+# Lab / Hawk
+# set -x fish_color_prompt B5FFCE
+# set -x fish_color_cwd 7CFFAA
+# set -x fish_command_timer_color 82826E
+
+# Science
+set -x fish_color_prompt 383838
+set -x fish_color_cwd C4A980
+set -x fish_command_timer_color C4C4C4
 
 set -x FZF_DEFAULT_COMMAND "fd --type f --follow --hidden --exclude .git"
 set -x FZF_DEFAULT_OPTS " --bind 'ctrl-q:select-all+accept,tab:print-query'"
