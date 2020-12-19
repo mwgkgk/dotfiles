d7659d546e42442b5ca4b6b710ba21b29ac11041
mwgkgk <mwgkgk@gmail.com>
2020-12-19 12:50:05 +0200

    When running from Vim, don't change the title

--- fish/config/fish_title.fish
+++ fish/config/fish_title.fish
@@ -1,12 +1,14 @@
 function fish_title
-  set -l job_count (jobs -c | wc -l)
-  if test $job_count -eq 0
-    if [ "fish" != $_ ]
-      test -n $TMUX ;and tmux rename-window "$argv" 
-    else
-      test -n $TMUX ;and tmux rename-window (prompt_pwd)"/\$"
-    end
-  end
+  # When running from Vim, don't change the title:
+  if test -z "$VIM_SERVERNAME"
+      set -l job_count (jobs -c | wc -l)
+      if test $job_count -eq 0
+        if [ "fish" != $_ ]
+          test -n $TMUX ;and tmux rename-window "$argv" 
+        else
+          test -n $TMUX ;and tmux rename-window (prompt_pwd)"/\$"
+        end
+      end
 
   echo $_
 end
