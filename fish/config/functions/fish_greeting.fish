9bf6c0cbdb0e1ac1817cdbaf789ac79757ccbf6d
mwgkgk <mwgkgk@gmail.com>
2021-06-02 04:28:22 +0300

    Fix servus

--- fish/config/functions/fish_greeting.fish
+++ fish/config/functions/fish_greeting.fish
@@ -1,10 +1,8 @@
 function fish_greeting
-    # Servus broke rip
-
-    # if test -z "$VIM_SERVERNAME" &&
-    #     not test -z "$CHI" &&
-    #     not test "$CHI" = "rogue" &&
-    #     not test "$LISP_ADV"
-    #     servus
-    # end
+    if test -z "$VIM_SERVERNAME" &&
+        not test -z "$CHI" &&
+        not test "$CHI" = "rogue" &&
+        not test "$LISP_ADV"
+        servus
+    end
 end
