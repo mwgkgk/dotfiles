f0228287cd2a70dbcfbb0eb729b33cf1194f63c5
mwgkgk <mwgkgk>
2023-01-21 08:59:03 +0200

    Add rbenv

--- fish/config.fish
+++ fish/config.fish
@@ -55,3 +55,6 @@ eval (perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)
 
 # Haskell
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH $HOME/.ghcup/bin # ghcup-env
+
+# Ruby
+status --is-interactive; and rbenv init - fish | source
