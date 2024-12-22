d22cef3429d836023ba3f4a7af2d65634458be22
mwgkgk <mwgkgk@gmail.com>
2024-12-23 01:51:34 +0200

    Display frecent sessions in fish greeting

--- fish/config/functions/fish_greeting.fish
+++ fish/config/functions/fish_greeting.fish
@@ -4,5 +4,10 @@ function fish_greeting
         not test "$CHI" = "rogue" &&
         not test "$LISP_ADV"
         servus
+        set sessions (v all-frecent-sesh | head -n 7 | awk -F/ '{print $NF}' | tr '\n' ' ')
+
+        if test -n "$sessions"
+            echo -s (set_color $fish_command_timer_color) "x " (set_color normal) "$sessions"
+        end
     end
 end
