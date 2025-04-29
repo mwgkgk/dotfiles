00c873cda3b825c227a34def00cf1da231c6774f
mwgkgk <mwgkgk@gmail.com>
2025-04-29 03:07:22 +0300

    Add XDG

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,15 @@ set -x PATH ~/bin /usr/local/bin ~/.local/bin ~/.cargo/bin ~/.node_modules/bin ~
 umask 0007
 
 # ||
+# || XDG
+# ||
+
+set -x XDG_CONFIG_HOME $HOME/.config
+set -x XDG_DATA_HOME $HOME/.local/share
+set -x XDG_CACHE_HOME $HOME/.cache
+set -x XDG_STATE_HOME $HOME/.local/state
+
+# ||
 # || Colors
 # ||
 
